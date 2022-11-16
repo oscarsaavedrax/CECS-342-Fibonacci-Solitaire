70c2b96f09a9a2513a9d1e978dabbfc0b8116e86
oscarsaavedrax <103982163+oscarsaavedrax@users.noreply.github.com>
2022-11-15 17:50:30 -0800

    Create list for a new deck and a function to print the deck

--- fiboSolitaire.lisp
+++ fiboSolitaire.lisp
@@ -11,7 +11,13 @@
 (setq *print-case* :capitalize)
 
 ; Variable for menu selection
-(defvar *menuChoice*)
+(defvar *menu-choice*)
+
+; List to hold a new deck
+(defparameter *new-deck* '("AS" "2S" "3S" "4S" "5S" "6S" "7S" "8S" "9S" "10S" "JS" "QS" "KS" 
+                    "AC" "2C" "3C" "4C" "5C" "6C" "7C" "8C" "9C" "10C" "JC" "QC" "KC"
+                    "AD" "2D" "3D" "4D" "5D" "6D" "7D" "8D" "9D" "10D" "JD" "QD" "KD"
+                    "AH" "2H" "3H" "4H" "5H" "6H" "7H" "8H" "9H" "10H" "JH" "QH" "KH"))
 
 ; Function to print the menu
 (defun print-menu ()
@@ -22,18 +28,24 @@
     (format t "4) Play Solitarie~%")
     (format t "5) Exit~%")
     (terpri))
+
+; Function to print the deck
+(defun print-deck (deck)
+    (terpri)
+    (loop for x in deck do
+        (format t "~s, " x)))
     
 ; Print menu
 (print-menu)
 ; Get user choice for menu
 (format t "Please enter your choice (1-5): ")
-(setq menuChoice (read))
+(setq menu-choice (read))
 
 ; Loop menu options until user exits
 (loop
-    (when (eq menuChoice 5) (return))
+    (when (eq menu-choice 5) (return))
     ; Branch to menu selection
-    (case menuChoice 
+    (case menu-choice 
         (1 (format t "You selected New Deck~%"))
         (2 (format t "You selected Display Deck~%"))
         (3 (format t "You selected Shuffle Deck~%"))
@@ -44,5 +56,5 @@
     (print-menu)
     ; Get user choice for menu
     (format t "Please enter your choice (1-5): ")
-    (setq menuChoice (read))
+    (setq menu-choice (read))
 )
