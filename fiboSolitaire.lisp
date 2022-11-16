3d12dc17346980fd5030e61813c2496c957857ce
oscarsaavedrax <103982163+oscarsaavedrax@users.noreply.github.com>
2022-11-15 18:10:34 -0800

    Create function for selecting menu choice 1

--- fiboSolitaire.lisp
+++ fiboSolitaire.lisp
@@ -12,12 +12,14 @@
 
 ; Variable for menu selection
 (defvar *menu-choice*)
-
+; Variable for the playing deck
+(defparameter *playing-deck* nil)
 ; List to hold a new deck
 (defparameter *new-deck* '("AS" "2S" "3S" "4S" "5S" "6S" "7S" "8S" "9S" "10S" "JS" "QS" "KS" 
                     "AC" "2C" "3C" "4C" "5C" "6C" "7C" "8C" "9C" "10C" "JC" "QC" "KC"
                     "AD" "2D" "3D" "4D" "5D" "6D" "7D" "8D" "9D" "10D" "JD" "QD" "KD"
                     "AH" "2H" "3H" "4H" "5H" "6H" "7H" "8H" "9H" "10H" "JH" "QH" "KH"))
+
 
 ; Function to print the menu
 (defun print-menu ()
@@ -31,30 +33,42 @@
 
 ; Function to print the deck
 (defun print-deck (deck)
-    (terpri)
     (loop for x in deck do
-        (format t "~s, " x)))
+        (format t "~s, " x))
+    (terpri))
+
+; Function to set the new-deck equal to playing deck
+(defun selected-new-deck (new-deck)
+    (format t "You selected a New Deck~%")
+    (format t "Here is the new unshuffled deck~%")
+    (setf *playing-deck* (copy-list new-deck))
+    (print-deck *playing-deck*)
+    (terpri))
     
 ; Print menu
 (print-menu)
 ; Get user choice for menu
 (format t "Please enter your choice (1-5): ")
 (setq menu-choice (read))
+(terpri)
 
 ; Loop menu options until user exits
 (loop
     (when (eq menu-choice 5) (return))
     ; Branch to menu selection
     (case menu-choice 
-        (1 (format t "You selected New Deck~%"))
+        (1 (selected-new-deck *new-deck*))
         (2 (format t "You selected Display Deck~%"))
         (3 (format t "You selected Shuffle Deck~%"))
         (4 (format t "You selected Play Solitarie~%"))
-        (5 (format t "You selected Exit~%")))
+        (5 ()))
 
     ; Print menu
     (print-menu)
     ; Get user choice for menu
     (format t "Please enter your choice (1-5): ")
     (setq menu-choice (read))
+    (terpri)
 )
+
+(format t "Thank you for playing Fibonacci Solitarie!~%")
